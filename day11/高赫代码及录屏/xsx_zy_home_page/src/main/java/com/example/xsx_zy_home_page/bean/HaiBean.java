9228a94f98cba500727c5008b95cc6bcddeafb95
gaohe <1461729301@qq.com>
2020-12-28 22:11:13 +0800

    高赫作业011

---
+++ day11/高赫代码及录屏/xsx_zy_home_page/src/main/java/com/example/xsx_zy_home_page/bean/HaiBean.java
@@ -0,0 +1,18 @@
+package com.example.xsx_zy_home_page.bean;
+
+public class HaiBean {
+    private int imgPath;
+
+    public HaiBean(int imgPath) {
+        this.imgPath = imgPath;
+    }
+    public HaiBean(){}
+
+    public int getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(int imgPath) {
+        this.imgPath = imgPath;
+    }
+}
---
+++ day11/高赫代码及录屏/xsx_zy_home_page/src/main/res/screenMatchPX.bat
@@ -0,0 +1,2 @@
+java -jar %~dp0\screenMatchPX.jar 720 1280 1080,1776;1260,1760;
+pause
