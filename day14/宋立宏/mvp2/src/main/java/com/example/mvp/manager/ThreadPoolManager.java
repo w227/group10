5d9b9ced65835df8c4aa1c9b9404e31e56b26bf3
songlihong <17691140923@qq.com>
2021-01-03 07:50:39 +0800

    传知

---
+++ day14/宋立宏/mvp2/src/main/java/com/example/mvp/manager/ThreadPoolManager.java
@@ -0,0 +1,26 @@
+package com.example.mvp.manager;
+
+
+import com.example.mvp.base.BaseThreadPool;
+import com.example.mvp.thread.CustomThreadPool;
+import com.example.mvp.thread.SchduleThreadPool;
+import com.example.mvp.thread.SingleThreadPool;
+
+public class ThreadPoolManager {
+    public static final int CUSTOM_THREADPOOL = 0;
+    public static final int SINGLE_THREADPOOL = 1;
+    public static final int SCHDULE_THREADPOOL = 2;
+
+    public static BaseThreadPool getThreadPool(int type) {
+        switch (type) {
+            case CUSTOM_THREADPOOL:
+                return CustomThreadPool.getThreadPool();
+            case SINGLE_THREADPOOL:
+                return SingleThreadPool.getSingleThreaPool();
+            case SCHDULE_THREADPOOL:
+                return SchduleThreadPool.getmSchduleThreadPool();
+        }
+        return null;
+    }
+
+}
